b13291d4e195f56fda43aa71c39f773812af3c60
vguenoff <vgenoff@gmail.com>
2017-06-02 12:31:24 +0300

    styles finished

--- src/components/DriverDetail.js
+++ src/components/DriverDetail.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 
 
 const DriverDetail = ({ driverData, history }) => {
-  // const picUrl = `../img/drivers/${driverData.driverId}.png`;
   const avatar = require(`../img/drivers/${driverData.driverId}.png`);
   const name = `${driverData.Drivers[0].givenName} ${driverData.Drivers[0].familyName}`;
 
@@ -13,24 +12,33 @@ const DriverDetail = ({ driverData, history }) => {
   localStorage.setItem('history', JSON.stringify(b));
 
   return (
-    <div className="DriverDetail">
-      <img src={avatar} alt={name} />
-      <p>{name}</p>
-      <hr />
-      <p>Nationality: {driverData.Drivers[0].nationality}</p>
-      <p>Date Of Birth: {driverData.Drivers[0].dateOfBirth}</p>
-      <p>Starting Number: {driverData.Drivers[0].permanentNumber}</p>
-      <p>Biography: {driverData.Drivers[0].url}</p>
-      <ul>
-        {
-          a.map((item, i) => (
-            <li key={i}>
-              <img src={item[0]} alt=""/>
-              <span>{item[1]}</span>
-            </li>
-          ))
-        }
-      </ul>
+    <div>
+      <div className="DriverDetail">
+        <img src={avatar} alt={name} />
+        <h3><span>{name}</span></h3>
+        <hr />
+        <p>Nationality: {driverData.Drivers[0].nationality}</p>
+        <p>Date Of Birth: {driverData.Drivers[0].dateOfBirth}</p>
+        <p>Starting Number: {driverData.Drivers[0].permanentNumber}</p>
+        <p>Biography:&nbsp;
+          <Link to={driverData.Drivers[0].url} target="_blank">
+            {driverData.Drivers[0].url}
+          </Link>
+        </p>
+      </div>
+      <div className="history">
+        <p>Recently Viewed Drivers:</p>
+        <ul>
+          {
+            a.map((item, i) => (
+              <li key={i}>
+                <img src={item[0]} alt={item[1]} />
+                <h4><span>{item[1]}</span></h4>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 };
